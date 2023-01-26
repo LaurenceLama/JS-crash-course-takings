fd9a211c5084e639b0e7cd5d7e12e42b3614bcc6
LaurenceLama <laurencelamaFSE@gmail.com>
2023-01-26 20:18:17 +0800

    Initialized Data types & variables, doing the exercise later

---
+++ script.js
@@ -0,0 +1,54 @@
+//Data types and variables::::
+
+// STRINGSSS
+
+console.log("Hello world");
+
+console.log("Hello " + "worlders " + "100k");
+
+console.log("Hello World"[10]);
+
+// Shows how many characters
+console.log("Lolowlo".length);
+
+// Shows how to input any last letter to any text
+let str = "Laurence";
+console.log(str[str.length - 1]);
+
+
+// to know what data type when forgetful
+typeof "anything really,btw put this in the console of inspect element";
+// or
+typeof anything;
+
+
+// moving on to NUMBERSS
+
+15 % 5; //input this on inspect element (or not i know yu no) -basically it says the divisibility of a number, if result is not 0, it is the remainder
+
+
+// BOOOOOLEAN
+
+1 > 2 //input on f12
+2 > 1 //also this, 
+
+
+// NULLL and UNDEFINED, no codes I guess
+
+
+// & variables, I thought we go to conditionals but o well
+/** 
+ * Basically variables stores data like those I professionally, intricatively encoded codes above
+ * 3 ways to define variables:
+ * var - super outdated(don't use)
+ * let - can be used anywhere, everytime(I think)
+ * const - cannot be reassigned, altho kinda same with let
+*/
+
+const name = 'Laurence'
+let temperature = 20
+let isRaining = true
+
+temperature = temperature * 2
+
+console.log(temperature)
